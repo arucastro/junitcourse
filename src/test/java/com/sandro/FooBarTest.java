af9b656b974547daf641bd9b0c023ab68e4e8f62
sandro <sandrosilva9029@gmail.com>
2025-06-21 11:38:57 -0300

    first test created

---
+++ src/test/java/com/sandro/FooBarTest.java
@@ -0,0 +1,13 @@
+package com.sandro;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class FooBarTest {
+
+    @Test
+    void test() {
+        //fail("not implemented yet");
+        System.out.println("Hello world");
+    }
+}
