954d0092ef03d1f6d4938de3dc0d74aefe6bdb2a
sandro <sandrosilva9029@gmail.com>
2025-06-25 21:55:11 -0300

    changed message to lazy assert message

--- src/test/java/com/sandro/math/SimpleMathTest.java
+++ src/test/java/com/sandro/math/SimpleMathTest.java
@@ -10,8 +10,10 @@ class SimpleMathTest {
     @Test
     void testSum() {
         SimpleMath math = new SimpleMath();
-        Double actual = math.sum(6.2D, 2D);
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+        Double actual = math.sum(firstNumber, secondNumber);
         Double expected = 8.2;
-        Assertions.assertEquals(expected, actual, "Test did not produced expected results");
+        Assertions.assertEquals(expected, actual, () -> firstNumber + "+" + secondNumber + " did not produce " + expected);
     }
 }
